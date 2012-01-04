1f67923e851d1dbdbdd2fb09644d3e6567872929
Julian Qian <jqian@jqian.net>
2012-01-04 08:55:04 +0000

    Trival updates for posts list style.
    hmmm, I commited a buggy version last night ...

--- tumblr-mode.el
+++ tumblr-mode.el
@@ -1,39 +1,39 @@
-;;; -*- indent-tabs-mode: t; tab-width: 8 -*-
-;;;
 ;;; tumblr-mode.el --- Major mode for Tumblr
-
+;;
 ;; Copyright (C) 2011 Julian Qian
-
+;;
 ;; Author: Julian Qian <junist@gmail.com>
 ;; Created: Dec 25, 2011
-;; Keywords: tumblr blog writer
-
+;; Version: 0.1
+;;
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
-
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
-
+;;
 ;;; Commentary:
-
+;;
 ;; tumblr-mode.el is a major mode for tumblr.com.
-;; You can write and manage your tumblr posts in Emacs.
-
+;;
+;; You can write and manage your tumblr posts in Emacs with this mode.
+;; It only supports to create regular(text) type in tumblr.com now,
+;; because I think it's the most suitable type that can be edited in
+;; Emacs.
+;;
 ;; - `tumblr-new-post' create a buffer to post
 ;; - `tumblr-save-post' save buffer to tumblr.com
 ;; - `tumblr-list-posts' list tumblr posts in a buffer
-
-;;; Feature Request:
-
+;;
 
 ;;; Code:
 
@@ -64,10 +64,16 @@
 (defun assqref (item alist)
   (cdr (assq item alist)))
 
+(defun tumblr-format-timestamp (secs)
+  (format-time-string "%F" (seconds-to-time (if (stringp secs)
+                                                (string-to-number secs)
+                                              secs))))
+
 (defun tumblr-get-posts-count ()
-  (let* ((root (tumblr-http-get-xml-root
-                (format "http://%s/api/read?num=1&filter=text"
-                        tumblr-hostname)))
+  (let* ((root (tumblr-authenticated-read-xml-root
+                tumblr-hostname
+                '(("num" . "1")
+                  ("filter" . "none"))))
          (tumblr (car root))
          (posts (car (xml-get-children tumblr 'posts)))
          (attrs (xml-node-attributes posts))
@@ -140,9 +146,9 @@
                               (search-forward-regexp "\r?\n\r?\n" nil t)
                               (let* ((beg (match-end 0))
                                      (end (point-max))
-                                     (post-id (string-to-number (buffer-substring beg end))))
+                                     (post-id (buffer-substring beg end)))
                                 ;; get post-id
-                                (message "Post OK. return id: %d" post-id)))
+                                (message "Post OK. return id: %s" post-id)))
                              ((string= "403" status-code)
                               (message "Failed. Your email address or password were incorrect."))
                              ((string= "400" status-code)
@@ -157,24 +163,23 @@
                 tumblr-hostname
                 `(("num" . ,(number-to-string retrieving))
                   ("filter" . "none")
-                  ("tagged" . tagged)
-                  ("state" . state))))
+                  ("tagged" . ,tagged)
+                  ("state" . ,state))))
          (tumblr (car root))
          (posts (car (xml-get-children tumblr 'posts)))
          (post-list (xml-get-children posts 'post)))
     (mapcar (lambda (post)
               (let* ((attrs (xml-node-attributes post))
                      (timestamp (assqref 'unix-timestamp attrs))
-                     (date (assqref 'date attrs))
+                     ;; (date (assqref 'date attrs))
                      (id (assqref 'id attrs))
                      (slug (assqref 'slug attrs))
                      (url (assqref 'url attrs))
-                     (title (cddar (xml-get-children post 'regular-title)))
+                     (title (caddar (xml-get-children post 'regular-title)))
                      (tags (mapcar (lambda (tag) (caddr tag))
                                    (xml-get-children post 'tag))))
                 ;; return a list of alist
                 `((timestamp    .   ,timestamp)
-                  (date         .   ,date)
                   (id           .   ,id)
                   (slug         .   ,slug)
                   (url          .   ,url)
@@ -182,52 +187,65 @@
                   (tags         .   ,tags))))
             post-list)))
 
+(defun tumblr-list-posts-format (width content)
+  (let ((fmt (format "%%-%d.%ds" width width)))
+    (format fmt content)))
+
 (defun tumblr-list-posts (&optional tagged state)
   (interactive)
   (let* ((total (tumblr-get-posts-count))
-           (total-retrieving (or (if (> tumblr-retrieve-posts-num-total total)
-                                     total
-                                   tumblr-retrieve-posts-num-total) total))
-           (remaining total-retrieving)
-           (tumblr-retrieve-posts-list nil))
-      (while (> remaining 0)
-        (let ((retrieving (if (> remaining tumblr-retrieve-posts-num-once)
-                              tumblr-retrieve-posts-num-once
-                            remaining)))
-          (setq remaining (- remaining retrieving))
-          (setq tumblr-retrieve-posts-list
-                (append tumblr-retrieve-posts-list
-                        (tumblr-get-posts-list-internal retrieving tagged state)))))
-      ;; list all posts
-      (with-current-buffer (get-buffer-create "*tumblr-mode*")
-        (tumblr-mode-setup)
-        ;; (toggle-read-only 1)
-        (goto-char (point-min))
-        (save-excursion
-          (kill-region (point-min) (point-max))
+         (total-retrieving (or (if (> tumblr-retrieve-posts-num-total total)
+                                   total
+                                 tumblr-retrieve-posts-num-total) total))
+         (remaining total-retrieving)
+         (tumblr-retrieve-posts-list nil))
+    (while (> remaining 0)
+      (let ((retrieving (if (> remaining tumblr-retrieve-posts-num-once)
+                            tumblr-retrieve-posts-num-once
+                          remaining)))
+        (setq remaining (- remaining retrieving))
+        (setq tumblr-retrieve-posts-list
+              (append tumblr-retrieve-posts-list
+                      (tumblr-list-posts-internal retrieving tagged state)))))
+    ;; list all posts
+    (with-current-buffer (get-buffer-create "*tumblr-mode*")
+      (tumblr-mode-setup)
+      ;; (toggle-read-only 1)
+      (goto-char (point-min))
+      (save-excursion
+        (kill-region (point-min) (point-max))
+        (let ((title-len 50)
+              (tags-len  20)
+              (date-len  10)
+              (ov (make-overlay (line-beginning-position) (line-end-position))))
           ;; header
           (save-excursion
-            (insert "         Title                           Tags         Date     \n"))
-          (let ((ov (make-overlay (line-beginning-position) (line-end-position))))
-            (overlay-put ov 'face 'header-line))
+            (insert (tumblr-list-posts-format title-len "Title"))
+            (insert (tumblr-list-posts-format tags-len "Tags"))
+            (insert (tumblr-list-posts-format date-len "Date"))
+            (insert "\n"))
+          (overlay-put ov 'face 'header-line)
           (forward-line)
-          ;; list
+          ;; list posts
           (mapc (lambda (post)
-                  (insert (assqref 'title post))
-                  (insert (mapconcat (lambda (tag) (format "#%s" tag))
-                                     (assqref 'tags post) ", "))
-                  (insert (assqref 'date post))
+                  (insert (tumblr-list-posts-format
+                           title-len (assqref 'title post)))
+                  (insert (tumblr-list-posts-format
+                           tags-len (mapconcat (lambda (tag) (format "#%s" tag))
+                                               (assqref 'tags post) ", ")))
+                  (insert (tumblr-list-posts-format
+                           date-len (tumblr-format-timestamp (assqref 'timestamp post))))
                   (make-text-button (line-beginning-position) (line-end-position)
-                                      'id (assqref 'id post)
-                                      'action 'tumblr-get-post-edit
-                                      'face 'default)
+                                    'id (assqref 'id post)
+                                    'action 'tumblr-get-post-edit
+                                    'face 'default)
                   (insert "\n"))
-                tumblr-retrieve-posts-list))
+                tumblr-retrieve-posts-list)))
 
-        ;; skip header
-        (forward-line)
-        (toggle-read-only t)
-        (set-window-buffer nil (current-buffer)))))
+      ;; skip header
+      (forward-line)
+      (toggle-read-only t)
+      (set-window-buffer nil (current-buffer)))))
 
 (defun tumblr-get-post (post-id)
   (let* ((root (tumblr-authenticated-read-xml-root
@@ -238,28 +256,30 @@
          (posts (car (xml-get-children tumblr 'posts)))
          (post (car (xml-get-children posts 'post)))
          (attrs (xml-node-attributes post))
-         (title (cddar (xml-get-children post 'regular-title)))
-         (body (cddar (xml-get-children post 'regular-body))) ; post content
+         (title (caddar (xml-get-children post 'regular-title)))
+         (body (caddar (xml-get-children post 'regular-body))) ; post content
          (tags (mapcar (lambda (tag) (caddr tag))
-                       (xml-get-children post 'tag))))
+                       (xml-get-children post 'tag)))
+         (buffer (get-buffer-create (format "*tumbr: %s*" title))))
     ;; edit post
-    (with-current-buffer (get-buffer-create (format "*tumbr: %s*" title))
-      (tumblr-mode-post-setup)
+    (with-current-buffer buffer
+      (tumblr-mode-setup)
       (goto-char (point-min))
       (save-excursion
         (kill-region (point-min) (point-max))
         ;; insert meta info, as octopress does
-        (tumblr-insert-post-template title attrs)
+        (tumblr-insert-post-template title attrs tags)
         ;; insert content
-        (insert body)))))
+        (insert body)))
+    (switch-to-buffer buffer)))
 
-(defun tumblr-insert-post-template (title &optional attrs)
+(defun tumblr-insert-post-template (title &optional attrs-alist tags-list)
   (insert "--\n")
-  (let ((date (assqref 'date attrs))
-        (id (assqref 'id attrs))
-        (slug (assqref 'slug attrs))
-        (tags (assqref 'tags attrs))
-        (state (assqref 'state attrs)))
+  (let ((date (assqref 'date attrs-alist))
+        (id (assqref 'id attrs-alist))
+        (slug (assqref 'slug attrs-alist))
+        (state (assqref 'state attrs-alist))
+        (tags (mapconcat (lambda (tag) tag) tags-list ", ")))
     (and id (insert (format "id: %s\n" id)))
     (and title (insert (format "title: %s\n" title)))
     (and slug (insert (format "slug: %s\n" slug)))
@@ -293,14 +313,15 @@
                              (lines (split-string lines-text "\r?\n" t))
                              prop)
                         (mapcar (lambda (line)
-                                  (let ((meta (split-string line "[ :]" t)))
-                                    `(,(car meta) . ,(cadr meta))))
+                                  (when (string-match "\\([^:]+\\)[ \t]*:[ \t]*\\([^ ]+\\)" line)
+                                    `(,(match-string 1 line) . ,(match-string 2 line))))
                                 lines))))))
          ;; get body content
          (body (buffer-substring-no-properties body-start (point-max)))
          (id (assocref "id" props))
          (title (assocref "title" props))
-         (tags (assocref "tags" props)))
+         (tags (assocref "tags" props))
+         (date (assocref "date" props)))
     (if (y-or-n-p (format "%s post %s? tags: %s."
                           (if id "Save" "Create") title tags))
         (tumblr-write-post
@@ -308,14 +329,14 @@
          `(("post-id" . ,id)
            ("title" . ,title)
            ("body" . ,body)
-           ("tags" . ,tags))))))
+           ("tags" . ,tags)
+           ("date" . ,date))))))
 
 (defun tumblr-new-post (title)
   (interactive "sCreate post title: \n")
   (switch-to-buffer (format "*Tumblr: %s*" title))
   (tumblr-insert-post-template title
                                '((slug . " ")
-                                 (tags . " ")
                                  (state . "published")))
   (if (fboundp 'markdown-mode)
       (markdown-mode)
